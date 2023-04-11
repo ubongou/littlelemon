fad3132d4f73ce1fd022d06e3f95fb14e14a82ae
ubong <115485243+ubongou@users.noreply.github.com>
2023-04-11 19:27:11 -0400

    added serializers and booking url

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.decorators import api_view, permission_classes, throttle_classes
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+from django.core.paginator import Paginator, EmptyPage
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django.contrib.auth.models import User, Group
+from rest_framework.views import APIView
+from django.forms.models import model_to_dict
 
 # Create your views here.
 def index(request):
      return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
